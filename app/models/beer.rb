3e7ddfa85b33c916ceb33a2873711dc63b052421
Daniel Andrade <dam@unixmexico.org>
2021-04-01 09:40:08 -0500

    More Refactor

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -15,7 +15,7 @@ class Beer < ApplicationRecord
     response = response.first
     unless response.empty?
       beer = find_or_create_by(response)
-      user_beer = user.user_beers.find_or_create_by(beer: beer).update!(seen_at: Time.current.strftime('%Y-%m-%d %H:%M:%S'))
+      user_beer = user.user_beers.find_or_create_by(beer: beer)
     end
     response
   end
--- app/models/user_beer.rb
+++ app/models/user_beer.rb
@@ -2,6 +2,8 @@ class UserBeer < ApplicationRecord
   belongs_to :user
   belongs_to :beer
 
+  after_validation :set_seen_at, on: [:create, :update]
+
   def self.favorite_beer(beer_id, user)
     Beer.get_beer(beer_id, user)
     user_beer = find_by(beer_id: beer_id, user_id: user.id)
@@ -9,12 +11,16 @@ class UserBeer < ApplicationRecord
     return nil unless user_beer.present?
 
     user_beer.favorite = !user_beer.favorite
-    user_beer.seen_at = Time.current.strftime('%Y-%m-%d %H:%M:%S')
     user_beer.save
 
     user_beer.beer.attributes.merge!(
       favorite: user_beer.favorite
     )
+  end
+
+  private
+  def set_seen_at
+    self.seen_at = Time.current.strftime('%Y-%m-%d %H:%M:%S')
   end
 
 end
